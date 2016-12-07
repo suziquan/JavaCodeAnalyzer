95c4bac77b7bded273fbf16c4f42a31210836e04
SuZiquan <suziquan@outlook.com>
2016-12-08 00:55:38 +0800

    modified

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git add -A
 git commit -m "modified"
 git push
 mvn clean
-mvn deploy -DaltDeploymentRepository=suziquan-mvn-repo::default::file:d:/maven-repo/
+mvn deploy -DaltDeploymentRepository=suziquan-mvn-repo::default::file:d:/maven-repo/repository
 cd d:/maven-repo
 git add -A
 git commit -m "deploy"
